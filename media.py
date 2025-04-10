aeb9ca3b4c3630b7e195f2dca28eaa407368081a
Carolina Demori Rossato <crossato2147@gmail.com>
2025-04-09 22:45:46 -0300

    Add files via upload

---
+++ media.py
@@ -0,0 +1,5 @@
+n1=float(input('Digite a primeira nota: '))
+n2=float(input('Digite a segunda nota: '))
+n3=float(input('Digite a terceira nota: '))
+media=(n1*2+n2*3+n3*3)/10
+print('A media do aluno foi ',media)
