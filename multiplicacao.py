364ef70cfe55747932cf0655a904d75ec0947d5b
Carolina Demori Rossato <crossato2147@gmail.com>
2025-04-13 21:23:51 -0300

    Add files via upload

---
+++ multiplicacao.py
@@ -0,0 +1,4 @@
+n1=int(input('Digite o primeiro numero inteiro: '))
+n2=int(input('Digite o segundo numero inteiro: '))
+resultado=n1*n2
+print(f'O resultado da multiplicacao entre {n1} e {n2} e {resultado}')
